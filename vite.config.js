94e4c8231794e245a2412efcede74e83163e4c87
MeyiGi <faybmi7@gmail.com>
2026-04-03 21:11:12 +0600

    fix: vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://backend:8000', // Имя сервиса бэкенда в docker-compose
         changeOrigin: true,
-      },
-    },
-  },
+      }
+    }
+  }
 })
